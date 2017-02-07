07db2a62180886fb521ccb721eeb77a90311af91
Ada Lee <adalee2future@163.com>
2017-02-07 19:23:57 +0800

    dynamic title arrow and color to table divide to 2 Rmd sendmail with content and 2 html attachment css style right align some columns of tables

--- run.ksh
+++ run.ksh
@@ -2,7 +2,8 @@
 
 # sender and receiver can be any mail in host ele.me
 from=lili.li@ele.me
-to=lili.li@ele.me  #delimiter "," if multiple recipients
+to=fei.ren@ele.me,jiejun.gao@ele.me,ruiqing.zhang@ele.me,fanjing.lv@ele.me,diting.liu@ele.me,lili.li@ele.me  #delimiter "," if multiple recipients
+#to=lili.li@ele.me
 
 if [ $# -eq 0 ];
   then rpt_date=`date -v -1d '+%m%d'` # This could be only run in mac
@@ -18,14 +19,26 @@ then
   echo "Error, data file not exists!"
   exit -1
 fi
-	
 
-# generate Rmd file
-sed "s/DATE_MMDD/$rpt_date/g" template.Rmd > 异常交易监控日报$rpt_date.Rmd
 # generate report using R
-R -e "rmarkdown::render('异常交易监控日报$rpt_date.Rmd', output_format='html_document')"
+R -e "rmarkdown::render('abn_order_daily2.Rmd', output_format='html_document')"
+R --no-save << EOT
+rmarkdown::render('abn_order_daily2.Rmd',
+                  output_format='html_document',
+		  output_file='abn_ord_dly.$rpt_date.html',
+		  params=list(rpt_date="$rpt_date")
+)
+
+rmarkdown::render('abn_ord_dly_app.Rmd',
+                  output_format='html_document',
+		  output_file='abn_ord_dly_app.$rpt_date.html',
+		  params=list(rpt_date="$rpt_date")
+)
+
+EOT
+
+mv abn_ord_dly*.$rpt_date.html output/
 
 # send mail using python
 ./sendmail.py $rpt_date $from $to
 
-mv 异常交易监控日报$rpt_date.* output/
--- sendmail.py
+++ sendmail.py
@@ -4,30 +4,50 @@
 # params rpt_date, sender, receiver
 ## for example 0324 lili.li@ele.me lili.li@ele.me,jiejun.gao@ele.me
 import sys
+from pprint import pprint
 
 # mail imports
 import smtplib
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 rpt_date = sys.argv[1]
 me = sys.argv[2]
 you = sys.argv[3]
-print(sys.argv)
-print(rpt_date)
 
 # Create message container - the correct MIME type is multipart/alternative.
 msg = MIMEMultipart()
 msg['Subject'] = '异常交易监控日报%s' % rpt_date
 msg['From'] = me
 msg['To'] = you
+pprint(dict(msg))
 
-html_text = open("%s.html" % msg['Subject'], "rb").read()
+# html attachment 1
+html_text = open("output/abn_ord_dly.%s.html" % rpt_date, "rb").read()
 html = MIMEApplication(html_text)
-html['Content-Disposition'] = "attachment; filename*=''%E5%BC%82%E5%B8%B8%E4%BA%A4%E6%98%93%E7%9B%91%E6%8E%A7%E6%97%A5%E6%8A%A5" + rpt_date + ".html"
-
+html['Content-Disposition'] = "attachment; filename=abn_ord_dly.%s.html" % rpt_date
 msg.attach(html)
 
+# html attachment 1
+html_text2 = open("output/abn_ord_dly_app.%s.html" % rpt_date, "rb").read()
+html = MIMEApplication(html_text2)
+html['Content-Disposition'] = "attachment; filename=abn_ord_dly_app.%s.html" % rpt_date
+msg.attach(html)
+
+# mail body
+body='''
+
+
+异常交易监控日报：abn_ord_dly.%s.html
+附录（异常类型细分场景明细）：abn_ord_dly_app.%s.html
+
+Best regards,
+Operating Center BI team
+''' % (rpt_date, rpt_date)
+content = MIMEText(body, 'plain')
+msg.attach(content)
+
 s = smtplib.SMTP('email.ele.me')
-s.sendmail(me, you, msg.as_string())
+s.sendmail(me, you.split(","), msg.as_string())
 s.quit()
